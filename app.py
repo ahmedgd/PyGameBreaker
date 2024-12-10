9a1286e56b242e5227962691a7a87c97c96ca5b1
Ahmed Venam <wolf.venam@gmail.com>
2024-12-10 23:50:17 +0200

    Fix for Vercel deployment

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def start_game():
     return "Game Started!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
